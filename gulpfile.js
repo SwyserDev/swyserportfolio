675a3d08801af9cb6eeddc5df88884f081b96c47
Francois van der Merwe <francoisvandermerwe@standardbank.co.za>
2017-01-06 08:43:32 +0200

    added gulp clean and build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
 var cssmin = require('gulp-cssmin');
+var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('clean', function() {
+  gulp.src('dist/**/*', {read: false})
+    .pipe(clean());
+});
 
 gulp.task('minify-css', function() {
   gulp.src('css/*.css')
@@ -7,7 +16,46 @@ gulp.task('minify-css', function() {
     .pipe(gulp.dest('dist/css'));
 });
 
-
-gulp.task('delpoy-prod', function() {
-  // Stuff here
+gulp.task('fontAwesome', function(){
+  gulp.src('font-awesome/**/*')
+    .pipe(gulp.dest('dist/font-awesome'));
 });
+
+gulp.task('fonts', function() {
+  gulp.src('fonts/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('images', function() {
+  gulp.src('img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('js', function() {
+  gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('less', function() {
+  gulp.src('les/**/*')
+    .pipe(gulp.dest('dist/less'));
+});
+
+gulp.task('mail', function() {
+  gulp.src('mail/**/*')
+    .pipe(gulp.dest('dist/mail'));
+});
+
+gulp.task('html', function() {
+  gulp.src('index.html')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('root', function() {
+  gulp.src('favicon.ico')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', ['minify-css', 'fontAwesome', 'fonts', 'images', 'js', 'less', 'mail', 'html', 'root']);
